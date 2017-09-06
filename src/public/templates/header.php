5dbf4862c13a1add0a8d383e03ead17a1f5c9d96
Michaela Blackham <Michaelablackham@gmail.com>
2017-09-06 13:20:35 -0600

    setting up llocal environment and package revisions

---
+++ src/public/templates/header.php
@@ -0,0 +1,17 @@
+<header>
+  <nav>
+    <ul>
+      <li>Become a Member!</li>
+      <li>Sign In</li>
+    </ul>
+  </nav>
+  <nav>
+    <ul>
+      <li><a href="">About Us</a></li>
+      <li><a href="">Membership</a></li>
+      <li><a href="">Patriots Nation</a></li>
+      <li><a href="">Members</a></li>
+      <li><a href="">Contact Us</a></li>
+    </ul>
+  </nav>
+</header>
