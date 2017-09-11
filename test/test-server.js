05372a7a9c0fcb59aa6423a77f5d7435a9228c8d
Michaela Blackham <Michaelablackham@gmail.com>
2017-09-11 07:05:58 -0600

    revisions to testing to simply all the extra code

--- test/test-server.js
+++ test/test-server.js
@@ -10,119 +10,50 @@ var storage = server.storage;
 
 chai.use(chaiHttp);
 
+//Tests for each page
+describe('Node Capstone', function () {
+  //best practice is to add a semi colon if function starts with (), [], {}
+  //Create an array of pages to loop through and test
+  ;[
+    '/',
+    '/about.html',
+    '/contacts.html',
+    '/patriots-nation.html',
+    '/contact.html',
+    '/members.html',
+    '/membership.html',
+    '/profile.html',
+    '/sign-in.html'
+  ].forEach(route => {
+    //for each statement to go through each page and test if it is a status 200(ok)
+    //Pass in the route (string from array)
+    //Pass in the done() function, which we call at the end of each of these
+    it(route, done => {
+      chai.request(app)
+      //getting the route being passed instead of writing it in the get function
+      .get(route)
+      .end(function(err, res) {
+        //should get a 200 status as a response
+        res.should.have.status(200);
+        //should be html
+        res.should.be.html;
+        //always call another promise of done
+        done();
+      });
+    })
+  })
+})
+
+//ABOVE IS USED INSTEAD OF A BUNCH OF THESE TEST FUNCTIONS
 //Test For Index Page
-describe('index page', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For About Page
-describe('about page', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/about.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For Contact Listing Page
-describe('contact listing page', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/contacts.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For Pats Nation Page
-describe('nation page', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/patriots-nation.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For Contact Cllub Page
-describe('contact', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/contact.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For Members Only Page
-describe('members only', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/members.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For Membership Info Page
-describe('membership info', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/membership.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For Membership Info Page
-describe('user profile', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/profile.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
-
-//Test For Membership Info Page
-describe('sign in', function() {
-  it('exists', function(done) {
-    chai.request(app)
-      .get('/sign-in.html')
-      .end(function(err, res) {
-        res.should.have.status(200);
-        res.should.be.html;
-        done();
-    });
-  });
-});
+// describe('index page', function() {
+//   it('exists', function(done) {
+//     chai.request(app)
+//       .get('/')
+//       .end(function(err, res) {
+//         res.should.have.status(200);
+//         res.should.be.html;
+//         done();
+//     });
+//   });
+// });
